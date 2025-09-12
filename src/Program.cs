5d7f67aea7d7f75b80e4bbf6781db5295424143f
Daniel Kinney <97559791+daniel-kinney-seequent@users.noreply.github.com>
2025-09-12 15:37:49 +1200

    Initial commit
    This is some initial example code for working with the duf DLLs.
    
    Author: George Turczynski

---
+++ src/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using SharedCode;
+using Deswik.Entities.Cad;
+using Deswik.Entities;
+using Deswik.Duf;
+using Deswik.Serialization;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+
+namespace ConsoleAppNet46
+{
+    class Program
+    {
+        static Program()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += MyResolveEventHandler;
+        }
+
+        private static Assembly MyResolveEventHandler(object sender, ResolveEventArgs args)
+        {
+            string requestedAssemblyNameSimple = new AssemblyName(args.Name).Name;
+            string deswik_install_path = @"C:\DeswikSoftware\DeswikSuite\Binaries\Release64";
+
+            if (!Directory.Exists(deswik_install_path))
+            {
+                deswik_install_path = @"C:\Program Files\Deswik\Deswik.Suite 2024.1";
+            }
+
+            string fullPathToAssembly = Path.Combine(deswik_install_path, requestedAssemblyNameSimple + ".dll");
+            if (File.Exists(fullPathToAssembly))
+            {
+                Assembly assembly = Assembly.LoadFrom(fullPathToAssembly);
+                return assembly;
+            }
+            return null;
+        }
+
+        private static Layer GetLayer(DufDocument doc, string layer_str)
+        {
+            List<ItemHeader> layer_headers = doc.Duf.GetEntityHeadersWithName(Category.Layers, layer_str);
+            ItemHeader layer_header = layer_headers[0];
+            Guid layer_guid = layer_header.EntityGuid;
+
+            BaseEntity base_entity = doc.GetEntityByGuid(layer_guid);
+            Layer layer = base_entity as Layer;
+
+            return layer;
+        }
+
+        static void Main(string[] args)
+        {
+            string new_key = "new_key";
+            string new_value = "new_value";
+
+            // Setup the .duf file
+            var path = @"..\..\..\demo.duf";
+            var path2 = @"..\..\..\demo_with_attributes.duf";
+            File.Copy(path, path2, true);
+
+            using (var docBefore = new DufDocument(path2))
+            {
+                docBefore.LoadReferenceEntities();
+                docBefore.LoadModelEntities();
+
+                Layer layer = GetLayer(docBefore, "0");
+                var layer_xprops_before = layer.XProperties;
+
+                Console.WriteLine($"Key present before: {layer_xprops_before.ContainsKey(new_key)}");
+
+                // Trying to edit XProperties
+                var props_list = new List<PropValue>();
+                props_list.Add(new PropValue(new_value));
+                var xprop = new XProperty() { Name = new_key, /* NOTE: XProperty must be named! */ Value = props_list };
+                layer_xprops_before.Add(new_key, xprop);
+                var new_key_exists = layer.XProperties.ContainsKey(new_key);
+                var new_keys_value = new_key_exists ? layer.XProperties[new_key].Value.First().Value : "";
+
+                // Confirmed it's set in memory
+                Console.WriteLine($"Key present after: {new_key_exists}, value is '{new_keys_value}'");
+
+                // Trying to save changes
+                docBefore.Save();
+            }
+
+            // Reload the file.
+            using (var docAfter = new DufDocument(path2))
+            {
+                docAfter.LoadReferenceEntities();
+                //docAfter.LoadModelEntities(); // Uncomment to load model entities
+
+                Layer layer = GetLayer(docAfter, "0");
+                var new_key_exists = layer.XProperties.ContainsKey(new_key);
+                var new_keys_value = new_key_exists ? layer.XProperties[new_key].Value.First().Value : "";
+
+                Console.WriteLine($"Key present after reload: {new_key_exists}, value is '{new_keys_value}'");
+            }
+        }
+    }
+}
---
+++ src/SimpleDuf.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+using Deswik.Core.Structures;
+using Deswik.Duf;
+using Deswik.Entities;
+using Deswik.Entities.Cad;
+using Deswik.Serialization;
+
+namespace SharedCode
+{
+    public class DufDocument : IDisposable
+    {
+        private static readonly Guid _documentPaletteGuid = new Guid("8876D917-BF46-4F20-B6CB-8410B642F784");
+
+        public string Path { get; protected set; }
+
+        public DufImplementation<Category> Duf { get; private set; }
+
+        private GuidReferences _guidReferences;
+
+        private Dictionary<Category, List<BaseEntity>> _entitiesByCategory;
+
+        private DocumentPalette _documentPalette;
+
+        private Document _document;
+
+        public DufDocument(string path)
+        {
+            Path = path;
+
+            Duf = new DufImplementation<Category>(Path, new Deswik.Entities.Cad.Activator(), new Deswik.Entities.Cad.Upgrader());
+
+            _guidReferences = new GuidReferences();
+
+            _entitiesByCategory = new Dictionary<Category, List<BaseEntity>>();
+
+            _entitiesByCategory[Category.Palette] = new List<BaseEntity>();
+            _entitiesByCategory[Category.LineTypes] = new List<BaseEntity>();
+            _entitiesByCategory[Category.Layers] = new List<BaseEntity>();
+            _entitiesByCategory[Category.Images] = new List<BaseEntity>();
+            _entitiesByCategory[Category.TextStyles] = new List<BaseEntity>();
+            _entitiesByCategory[Category.Blocks] = new List<BaseEntity>();
+            _entitiesByCategory[Category.DimStyles] = new List<BaseEntity>();
+            _entitiesByCategory[Category.HatchPatterns] = new List<BaseEntity>();
+            _entitiesByCategory[Category.Lights] = new List<BaseEntity>();
+            _entitiesByCategory[Category.ModelEntities] = new List<BaseEntity>();
+            _entitiesByCategory[Category.Document] = new List<BaseEntity>();
+        }
+
+        public void Dispose()
+        {
+            Duf?.Dispose();
+        }
+
+        public BaseEntity GetEntityByGuid(Guid guid)
+        {
+            return _guidReferences?.GetEntity(guid);
+        }
+
+        public void LoadModelEntities(Guid? layerGuid = null)
+        {
+            var loadedItems = new ConcurrentQueue<BaseEntity>();
+            List<Guid?> parents = null;
+
+            if (layerGuid != null)
+            {
+                parents = new List<Guid?> { layerGuid };
+            }
+
+            var loadTask = Task.Run(() => {
+                Duf.LoadFromLatestWithConcurrentQueue(loadedItems, _guidReferences, new FilterCriteria<Category> { ParentIds = parents, Categories = new List<Category> { Category.ModelEntities } }, false, true, null);
+            });
+
+            while (TaskNotFinished(loadTask) || !loadedItems.IsEmpty)
+            {
+                if (!loadedItems.TryDequeue(out var entity))
+                {
+                    System.Threading.Thread.Sleep(10);
+                    continue;
+                }
+
+                AddLoadedEntity(Category.ModelEntities, entity);
+            }
+        }
+
+        public void LoadReferenceEntities()
+        {
+            LoadTopLevelEntitiesOfType(Category.Palette);
+            LoadTopLevelEntitiesOfType(Category.LineTypes);
+            LoadTopLevelEntitiesOfType(Category.Layers);
+            LoadTopLevelEntitiesOfType(Category.Images);
+            LoadTopLevelEntitiesOfType(Category.TextStyles);
+            LoadTopLevelEntitiesOfType(Category.Blocks);
+            LoadTopLevelEntitiesOfType(Category.DimStyles);
+            LoadTopLevelEntitiesOfType(Category.HatchPatterns);
+            LoadTopLevelEntitiesOfType(Category.Lights);
+            LoadTopLevelEntitiesOfType(Category.Document);
+
+            if (_entitiesByCategory.TryGetValue(Category.Palette, out var paletteEntities))
+            {
+                _documentPalette = paletteEntities.OfType<DocumentPalette>().First();
+            }
+
+            if (_entitiesByCategory.TryGetValue(Category.Document, out var documentEntities))
+            {
+                _document = documentEntities.OfType<Document>().First();
+            }
+        }
+
+        public void Save(string saveAsPath = null)
+        {
+            var existingDocGuid = Duf?.Header != null ? Duf.LoadFromLatest(null, new FilterCriteria<Category> { Categories = new List<Category> { Category.Document } }, true, false, null).FirstOrDefault()?.Guid : null;
+            Duf.Save(GetSaveSets(existingDocGuid), GetUserName(), saveAsPath, true);
+        }
+
+        private void AddLoadedEntity(Category category, BaseEntity entity)
+        {
+            if (!_entitiesByCategory.TryGetValue(category, out var baseEntities))
+            {
+                baseEntities = new List<BaseEntity>();
+                _entitiesByCategory.Add(category, baseEntities);
+            }
+
+            baseEntities.Add(entity);
+        }
+
+        /// <summary>
+        ///     Returns true if the provided task is still executing
+        /// </summary>
+        /// <param name="task"></param>
+        /// <returns></returns>
+        private static bool TaskNotFinished(Task task)
+        {
+            return task.Status != TaskStatus.Canceled &&
+                   task.Status != TaskStatus.Faulted &&
+                   task.Status != TaskStatus.RanToCompletion;
+        }
+
+        private void LoadTopLevelEntitiesOfType(Category category)
+        {
+            var loadedItems = new ConcurrentQueue<BaseEntity>();
+            var loadTask = Task.Run(() => {
+                Duf.LoadFromLatestWithConcurrentQueue(loadedItems, _guidReferences, new FilterCriteria<Category> { Categories = new List<Category> { category } }, false, true, null);
+            });
+
+            while (TaskNotFinished(loadTask) || !loadedItems.IsEmpty)
+            {
+                BaseEntity entity;
+                if (!loadedItems.TryDequeue(out entity))
+                {
+                    System.Threading.Thread.Sleep(10);
+                    continue;
+                }
+
+                AddLoadedEntity(category, entity);
+            }
+        }
+
+        private void SetDocumentPalette()
+        {
+            if (_documentPalette == null)
+            {
+                _documentPalette = new DocumentPalette
+                {
+                    Guid = _documentPaletteGuid
+                };
+
+                _documentPalette.Palette = new Palette(); // TODO: populate this with sensible values
+            }
+        }
+
+        private IEnumerable<SaveSet<Category>> GetSaveSets(Guid? existingDocGuid, bool maintainEntityOrdering = false)
+        {
+            var _guidReferences = new GuidReferences();
+
+            SetDocumentPalette();
+
+            yield return new SaveByEnumerableSet<Category>(new[] { new SaveEntityItem(_documentPalette) }, Category.Palette, true, _guidReferences, new PerformanceTweaking { NumberCpuThreads = 1 }, true);
+
+            yield return new SaveByEnumerableSet<Category>(_entitiesByCategory[Category.LineTypes].Select(x => new SaveEntityItem(x)), Category.LineTypes, true, _guidReferences, new PerformanceTweaking { NumberCpuThreads = 1 }, true);
+            yield return new SaveByEnumerableSet<Category>(_entitiesByCategory[Category.Images].Select(x => new SaveEntityItem(x)), Category.Images, true, _guidReferences, new PerformanceTweaking { NumberCpuThreads = 1 }, true);
+            yield return new SaveByEnumerableSet<Category>(_entitiesByCategory[Category.Layers].Select(x => new SaveEntityItem(x)), Category.Layers, true, _guidReferences, new PerformanceTweaking { NumberCpuThreads = 1 }, true);
+            yield return new SaveByEnumerableSet<Category>(_entitiesByCategory[Category.TextStyles].Select(x => new SaveEntityItem(x)), Category.TextStyles, true, _guidReferences, new PerformanceTweaking { NumberCpuThreads = 1 }, true);
+
+            var dufBlocks = new List<BaseEntity>();
+            yield return new SaveByEnumerableSet<Category>(_entitiesByCategory[Category.Blocks].Select(x =>
+            {
+                dufBlocks.Add(x);
+                return new SaveEntityItem(x);
+            }), Category.Blocks, true, _guidReferences, new PerformanceTweaking { NumberCpuThreads = 1 }, false);
+
+            // NOTE: we add block refs at end not during serialization
+            foreach (var block in dufBlocks)
+            {
+                _guidReferences.AddEntity(block);
+            }
+
+            yield return new SaveByEnumerableSet<Category>(_entitiesByCategory[Category.DimStyles].Select(x => new SaveEntityItem(x)), Category.DimStyles, true, _guidReferences, new PerformanceTweaking { NumberCpuThreads = 1 }, true);
+            yield return new SaveByEnumerableSet<Category>(_entitiesByCategory[Category.HatchPatterns].Select(x => new SaveEntityItem(x)), Category.HatchPatterns, true, _guidReferences, new PerformanceTweaking { NumberCpuThreads = 1 }, true);
+            yield return new SaveByEnumerableSet<Category>(_entitiesByCategory[Category.Lights].Select(x => new SaveEntityItem(x)), Category.Lights, true, _guidReferences, new PerformanceTweaking { NumberCpuThreads = 1 }, true);
+
+            var modelEntities = _entitiesByCategory[Category.ModelEntities].OfType<Figure>().ToArray();
+
+            yield return new SaveByIndexSet<Category>((entityData) =>
+            {
+                if (entityData.Position >= modelEntities.Length)
+                {
+                    entityData.Finished = true;
+                    return;
+                }
+
+                var entity = modelEntities[entityData.Position];
+
+                if (maintainEntityOrdering && entity is Figure figure)
+                {
+                    figure.FigureOrder = (uint)entityData.Position + 1;
+                }
+
+                entityData.Entity = entity;
+            },
+            (entityMetadata) =>
+            {
+                var figure = modelEntities[entityMetadata.Position];
+                var layerGuid = figure.Layer.Guid;
+                var header = Duf.GetEntityHeader(figure.Guid);
+
+                if (header is ItemHeader ih)
+                {
+                    if (ih.MinX is double minX && ih.MinY is double minY && ih.MinZ is double minZ && ih.MaxX is double maxX && ih.MaxY is double maxY && ih.MaxZ is double maxZ)
+                    {
+                        entityMetadata.EntityMetadata = new EntityMetadata(new Vector3_dp(minX, minY, minZ), new Vector3_dp(maxX, maxY, maxZ), layerGuid);
+                    }
+                }
+                else
+                {
+                    entityMetadata.EntityMetadata = new EntityMetadata(layerGuid);
+                }
+            }, Category.ModelEntities, false, _guidReferences, new PerformanceTweaking { NumberCpuThreads = 1 }, false);
+
+            if (_document.Guid.Equals(Guid.Empty))
+            {
+                _document.Guid = existingDocGuid ?? Guid.NewGuid();
+            }
+
+            yield return new SaveByEnumerableSet<Category>(new[] { new SaveEntityItem(_document) }, Category.Document, true, _guidReferences, new PerformanceTweaking { NumberCpuThreads = 1 }, true);
+        }
+
+        private static string GetUserName()
+        {
+            string username = null;
+
+            try
+            {
+                username = WindowsIdentity.GetCurrent().Name;
+            }
+            catch
+            {
+                try
+                {
+                    username = Environment.UserName;
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(username)) username = "unknown";
+
+            return username;
+        }
+    }
+}
