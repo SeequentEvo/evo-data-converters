faa31abc02a1913ce1aee57b99845d24901ff156
Daniel Kinney <97559791+daniel-kinney-seequent@users.noreply.github.com>
2025-09-15 14:16:29 +1200

    Make running of dotnet-install.ps1 more secure
    - Avoid using -ExecutionPolicy Bypass
    - The script is signed by Microsoft, so use -ExecutionPolicy RemoteSigned
    - Check that the name on the cert matches "Microsoft Corporation"

--- packages/duf/hatch_build.py
+++ packages/duf/hatch_build.py
@@ -7,7 +7,7 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 
 class ABCDEFGHIJIKLBuildHook(BuildHookInterface):
-    PLUGIN_NAME = 'custom'
+    PLUGIN_NAME = "custom"
 
     @property
     def dotnet_path(self):
@@ -36,16 +36,41 @@ class ABCDEFGHIJIKLBuildHook(BuildHookInterface):
     def initialize(self, version: str, build_data) -> None:
         self._install_local_dotnet_sdk()
         self._build_simple_duf()
+
+    @staticmethod
+    def _check_script_has_microsoft_cert(script: str):
+        """
+        Raises PermissionError if the cert does not belong to Microsoft Corporation. It doesn't check that the cert is
+        valid. That's left to `-ExecutionPolicy RemoteSigned`.
+        """
+        # This powershell command grabs the "CN" value from the "Subject" field of the certificate. It shows as
+        # "Microsoft Corporation", which is assumed to be stable and secure.
+        cmd = [
+            "powershell",
+            "-NoProfile",
+            "-Command",
+            f"(Get-AuthenticodeSignature {script}).SignerCertificate.GetNameInfo([System.Security.Cryptography.X509Certificates.X509NameType]::SimpleName,$false)",
+        ]
+        name = subprocess.run(cmd, capture_output=True, text=True).stdout.strip()
+        expected = "Microsoft Corporation"
+        if name != expected:
+            script_name = os.path.basename(script)
+            msg = f"Unexpected name on the certificate of {script_name}. Expected: {expected}, Got: {name}"
+            raise PermissionError(msg)
+
     def _install_local_dotnet_sdk(self):
         install_source = "https://dot.net/v1/dotnet-install.ps1"
 
         os.makedirs(self.dotnet_path, exist_ok=True)
         urllib.request.urlretrieve(install_source, self.dotnet_install_script)
 
+        self._check_script_has_microsoft_cert(self.dotnet_install_script)
+
         cmd = [
             "powershell",
+            "-NoProfile",
             "-ExecutionPolicy",
-            "Bypass",
+            "RemoteSigned",  # Powershell version 5.1 has a default ExecutionPolicy of Restricted
             "-NoLogo",
             "-NoProfile",
             "-File",
@@ -63,6 +88,6 @@ class ABCDEFGHIJIKLBuildHook(BuildHookInterface):
             "Release",
             "--output",
             self.simple_duf_output_bin,
-            self.simple_duf_csharp_project_file
+            self.simple_duf_csharp_project_file,
         ]
         subprocess.run(cmd)
