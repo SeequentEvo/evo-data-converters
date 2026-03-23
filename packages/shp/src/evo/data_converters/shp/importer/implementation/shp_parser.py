d4362e07eae26393582abb736f602abf44dccabf
Dawson Brown <176314370+DBrownSeequent@users.noreply.github.com>
2026-03-23 15:01:32 -0400

    Add shapefile to triangle mesh converter.

---
+++ packages/shp/src/evo/data_converters/shp/importer/implementation/shp_parser.py
@@ -0,0 +1,84 @@
+#  Copyright © 2026 Bentley Systems, Incorporated
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import shapefile
+
+from pathlib import Path
+
+from evo_schemas.objects.triangle_mesh import TriangleMesh_V2_2_0
+from evo_schemas.components import (
+    BoundingBox_V1_0_1,
+    Crs_V1_0_1
+)
+
+from evo.objects.utils.data import ObjectDataClient
+
+from evo.data_converters.shp.importer.exceptions import InvalidSHPError
+from evo.data_converters.shp.importer.implementation.mesh_builder import MeshBuilder
+
+class ShpParser:
+    """
+    Converts multipatch shapefiles (consiting of at least .shp, .shx, and .dbf files) to Triangle Mesh format.
+    Currently only supports multipatch files without rings.
+    """
+
+    def __init__(self, path: str, data_client: ObjectDataClient, crs: Crs_V1_0_1, tags: dict[str, str] = None):
+        """
+        Initialize the shapefile to triangle mesh converter.
+
+        :param path: The path to the input shapefile. Can be just the basename, a path to any component file, or
+        a path to a zip file containing the shapefile.
+        :param data_client: Object data client for uploading parquet files (real or stub).
+        :param crs: Coordiante reference system to use for the file. Cannot be None, but can be "unspecified".
+        :param tags: (Optional) Dict of tags to add to the Geoscience Object(s)."
+        """
+        self.path = path
+        self.data_client = data_client
+        self.crs = crs
+        self.tags = tags
+
+    def parse_shp(self) -> TriangleMesh_V2_2_0:
+        """
+        Open, validate, and parse the shapefile, converting and returning it as a Triangle Mesh.
+
+        :return: Triangle Mesh object representing the input shapefile.
+
+        :raise InvalidSHPError: If the input shapefile is invalid or cannot be parsed
+        """
+        with shapefile.Reader(self.path) as sf:
+            if sf.shapeType != shapefile.MULTIPATCH:
+                raise InvalidSHPError("Provided shapefile is not multipatch. Only multipatch shapefiles without rings are supported.")
+
+            mesh_builder = MeshBuilder(self.data_client, sf.fields[1:])
+
+            bounding_box = BoundingBox_V1_0_1(
+                min_x=sf.bbox[0],
+                min_y=sf.bbox[1],
+                min_z=sf.zbox[0] or 0.0,
+                max_x=sf.bbox[2],
+                max_y=sf.bbox[3],
+                max_z=sf.zbox[1] or 0.0
+            )
+
+            for shape_record in sf.iterShapeRecords():
+                mesh_builder.add_shape_record(shape_record)
+            
+            embedded_mesh = mesh_builder.build()
+
+            return TriangleMesh_V2_2_0(
+                triangles=embedded_mesh.triangles, 
+                parts=embedded_mesh.parts,
+                name=Path(self.path).stem,
+                uuid=None,
+                tags=self.tags,
+                bounding_box=bounding_box,
+                coordinate_reference_system=self.crs
+            )
---
+++ packages/shp/src/evo/data_converters/shp/importer/shp_to_evo.py
@@ -0,0 +1,91 @@
+#  Copyright © 2026 Bentley Systems, Incorporated
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import os
+
+from typing import TYPE_CHECKING, Optional
+
+from evo_schemas.objects.triangle_mesh import TriangleMesh_V2_2_0
+
+from evo.data_converters.common import (
+    EvoWorkspaceMetadata,
+    create_evo_object_service_and_data_client,
+    publish_geoscience_objects_sync,
+    crs_from_any
+)
+
+from evo.data_converters.shp.importer.implementation.shp_parser import ShpParser
+from evo.data_converters.shp.importer.implementation.local_data import LocalDataClient
+from evo.objects.data import ObjectMetadata
+
+if TYPE_CHECKING:
+    from evo.notebooks import ServiceManagerWidget
+
+def convert_shp(
+    filepath: str,
+    epsg_code: str,
+    evo_workspace_metadata: Optional[EvoWorkspaceMetadata] = None,
+    service_manager_widget: Optional["ServiceManagerWidget"] = None,
+    tags: Optional[dict[str, str]] = None,
+    upload_path: str = "",
+    publish_objects: bool = True,
+    overwrite_existing_objects: bool = False
+) -> list[TriangleMesh_V2_2_0] | list[ObjectMetadata]:
+    """
+    Convert an ESRI shapefile (.shp, .shx, and .dbf) to a triangle-mesh geoscience object.
+
+    :param filepath: Path to the base filename of the shapefile, any of the component files (.shp, .shx, or .dbf),
+     or a zip file containing the shapefile.
+    :param epsg_code: The EPSG code to use when creating a Coordinate Reference System object.
+    :param evo_workspace_metadata: (Optional) Evo workspace metadata.
+    :param service_manager_widget: (Optional) Service Manager Widget for use in jupyter notebooks.
+    :param tags: (Optional) Dict of tags to add to the Geoscience Object(s).
+    :param upload_path: (Optional) Path objects will be published under.
+    :param publish_objects: (Optional) Set False to prevent publishing and instead return Geoscience models.
+    :param overwrite_existing_objects: (Optional) Set True to overwrite any existing object at the destiation path.
+
+    :return: list[ObjectMetadata] if publish_objects is true, otherwise list[Regular2DGrid]
+
+    :raise MissingConnectionDetailsError: If no connections details could be derived.
+    :raise ConflictingConnectionDetailsError: If both evo_workspace_metadata and service_manager_widget present.
+    :raise InvalidOBJError: If the input shapefile is invalid or cannot be parsed.
+    :raise InvalidCRSError: If the input CRS information is invalid.
+
+    One of evo_workspace_metadata or service_manager_widget is required for publishing.
+    """
+    geoscience_objects = []
+
+    if publish_objects:
+        object_service_client, data_client = create_evo_object_service_and_data_client(
+            evo_workspace_metadata=evo_workspace_metadata, service_manager_widget=service_manager_widget
+        )
+    else:
+        data_client = LocalDataClient(upload_path)
+
+    full_tags = {
+        "Source": f"{os.path.basename(filepath)} (via Evo Data Converters)",
+        "Stage": "Experimental",
+        "InputType": "SHP",
+        **(tags or {}),
+    }
+
+    parser = ShpParser(path=filepath, data_client=data_client, crs=crs_from_any(epsg_code), tags=full_tags)
+    mesh = parser.parse_shp()
+
+    geoscience_objects.append(mesh)
+
+    objects_metadata = None
+    if publish_objects:
+        print("Publishing Shapefile")
+        objects_metadata = publish_geoscience_objects_sync(
+            geoscience_objects, object_service_client, data_client, upload_path, overwrite_existing_objects
+        )
+    
+    return objects_metadata if objects_metadata else geoscience_objects
