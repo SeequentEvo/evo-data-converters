4a77b4d81726b6cff1e1e29d792ad9c140a69bcb
AndersSondberg-Seequent <Anders.Sondberg@bentley.com>
2026-06-09 11:28:39 +0200

    CLGPHX-192 - Update to make shx and dbf filepaths explicit (#210)
    <!--
    Thank you for taking the time to make a pull request.
    
    Please review our [contribution
    guide](https://github.com/SeequentEvo/evo-data-converters/blob/main/CONTRIBUTING.md)
    and our
    [code of
    conduct](https://github.com/SeequentEvo/evo-data-converters/blob/main/CONTRIBUTING.md)
    before opening your first
    pull request.
    -->
    
    ## Description
    
    Updated the Shapefile Converter to now accept explicit paths for the
    companion files in the shapefile format (ie. shx and dbf).
    
    ## Checklist
    
    - [x] I have read the contributing guide and the code of conduct

--- packages/shp/src/evo/data_converters/shp/importer/shp_to_evo.py
+++ packages/shp/src/evo/data_converters/shp/importer/shp_to_evo.py
@@ -28,7 +28,13 @@ if TYPE_CHECKING:
 
 def convert_shp(
     filepath: str,
+    filepath_shx: str,
+    filepath_dbf: str,
     filepath_prj: Optional[str] = None,
+    filepath_cpg: Optional[str] = None,
+    filepath_sbn: Optional[str] = None,
+    filepath_sbx: Optional[str] = None,
+    filepath_xml: Optional[str] = None,
     evo_workspace_metadata: Optional[EvoWorkspaceMetadata] = None,
     service_manager_widget: Optional["ServiceManagerWidget"] = None,
     tags: Optional[dict[str, str]] = None,
@@ -39,9 +45,16 @@ def convert_shp(
     """
     Convert an ESRI shapefile (.shp, .shx, and .dbf) to a triangle-mesh geoscience object.
 
-    :param filepath: Path to the base filename of the shapefile, any of the component files (.shp, .shx, or .dbf),
-     or a zip file containing the shapefile.
-    :param epsg_code: The EPSG code to use when creating a Coordinate Reference System object.
+    :param filepath: Path to the .shp geometry file.
+    :param filepath_shx: Path to the .shx spatial index file.
+    :param filepath_dbf: Path to the .dbf attribute file.
+    :param filepath_prj: (Optional) Path to the .prj projection file. If not provided,
+     the CRS will be set to "unspecified".
+    :param filepath_cpg: (Optional) Path to the .cpg code page file. If provided, the encoding
+     specified in the file will be used when reading the .dbf attribute data.
+    :param filepath_sbn: (Optional) Path to the .sbn spatial index file. Accepted but not currently used.
+    :param filepath_sbx: (Optional) Path to the .sbx spatial index file. Accepted but not currently used.
+    :param filepath_xml: (Optional) Path to the .shp.xml metadata file. Accepted but not currently used.
     :param evo_workspace_metadata: (Optional) Evo workspace metadata.
     :param service_manager_widget: (Optional) Service Manager Widget for use in jupyter notebooks.
     :param tags: (Optional) Dict of tags to add to the Geoscience Object(s).
@@ -49,7 +62,7 @@ def convert_shp(
     :param publish_objects: (Optional) Set False to prevent publishing and instead return Geoscience models.
     :param overwrite_existing_objects: (Optional) Set True to overwrite any existing object at the destiation path.
 
-    :return: list[ObjectMetadata] if publish_objects is true, otherwise list[Regular2DGrid]
+    :return: list[ObjectMetadata] if publish_objects is true, otherwise list[TriangleMesh_V2_2_0]
 
     :raise MissingConnectionDetailsError: If no connections details could be derived.
     :raise ConflictingConnectionDetailsError: If both evo_workspace_metadata and service_manager_widget present.
@@ -76,7 +89,18 @@ def convert_shp(
 
     crs = prj_to_crs(filepath_prj)
 
-    parser = ShpParser(path=filepath, data_client=data_client, crs=crs, tags=full_tags)
+    parser = ShpParser(
+        path=filepath,
+        shx_path=filepath_shx,
+        dbf_path=filepath_dbf,
+        data_client=data_client,
+        crs=crs,
+        tags=full_tags,
+        cpg_path=filepath_cpg,
+        sbn_path=filepath_sbn,
+        sbx_path=filepath_sbx,
+        xml_path=filepath_xml,
+    )
     mesh = parser.parse_shp()
 
     geoscience_objects.append(mesh)
